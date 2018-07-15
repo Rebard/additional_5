80deb8f538ab11cb14c7f2329f18b5330c14d73e
Rebard <maximka2671@gmail.com>
2018-07-16 01:54:34 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
   var stack = [1,2,3], i = 0, count = 0;
-  var temp, var temp = "this is rename";
+  var temp;
   var flag = false;
   while(i < str.length)
   {
